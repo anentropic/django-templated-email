4c7245b67454ccbc22462768508598d6d3ead0a6
Paul Garner <paul.garner@imagination.com>
2013-08-30 19:47:21 +0100

    WIP

--- templated_email/backends/mandrill_backend.py
+++ templated_email/backends/mandrill_backend.py
@@ -20,24 +20,32 @@ class TemplateBackend(vanilla_django.TemplateBackend):
             template_suffix=None, template_dir=None, file_extension=None,
             extra_params=None, **kwargs):
 
+        # some internal basic defaults
+        config = {
+            'subject': _('%s email subject' % template_name),
+        }
+
         base_config = getattr(settings, 'TEMPLATED_EMAIL_MANDRILL', {})
-        config = base_config.get('_default', {})
+        # get general message defaults from settings:
+        config.update(base_config.get('_default', {}))
+        # get template-specific message defaults from settings:
         config.update(base_config.get(template_name, {}))
+
+        message = config.copy()
+        message = message.update({
+            'from_name': ' '.join(from_email.split(' ')[:-1]) or 'Nobody',
+            'from_email': from_email,
+            'to': recipient_list,
+        })
+        if 'message' not in context:
+            context['message'] = message
 
         parts = self._render_email(template_name, context,
                                    template_dir=template_prefix or template_dir,
                                    file_extension=template_suffix or file_extension)
-        message = {
-            'subject': config.get('subject', _('%s email subject' % template_name)) % context,
-            'html': parts.get('html', ''),
-            'text': parts.get('plain', ''),
-            'from_name': ' '.join(from_email.split(' ')[:-1]) or 'Nobody',
-            'from_email': from_email,
-            'to': recipient_list,
-            'track_opens': config.get('track_opens', False),
-            'track_clicks': config.get('track_clicks', False),
-            'tags': config.get('tags', []),
-        }
+
+        message['html'] = parts.get('html', ''),
+        message['text'] = parts.get('plain', ''),
         if cc:
             message['cc_address'] = ', '.join(cc)
         if bcc:
